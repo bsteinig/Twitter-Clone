dd8782d3c9c38f647ab7f7bd6117f267754f9f65
Benjamin Steinig <bsteinig@umich.edu>
2020-08-19 16:40:50 -0400

    Added first app named Chirps which creates a tweet page for each tweet in the tweets class

---
+++ chirps/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+
+from .models import Tweet
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    #return  HttpResponse("<h1>Welcome to Twitter Knock off</h>")
+    return render(request, "pages/home.html", context={},status=200)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by javascript
+    return json data
+    """
+
+    data =  {
+        "id": tweet_id,
+       # "image_path": obj.image.url
+    }
+    status = 200
+    try:
+     obj = Tweet.objects.get(id=tweet_id)
+     data['content'] = obj.content
+    except:
+        data['message']  = "Not Found"
+        status = 404
+
+    
+    return JsonResponse(data, status = status)
+    
---
+++ todo.md
@@ -0,0 +1,23 @@
+1.  Users
+    - Register
+    - Login
+    - Logout
+    - Profile
+        - Image
+        - Text
+        - Follow Button
+    - Feed
+2. Tweets
+    - Creating
+        - Text
+        - Image - Media storage server
+    - Delete
+    - Retweeting
+
+3. Following / Followers
+
+
+Long term todos
+- Notifications
+- DM
+- Explore
